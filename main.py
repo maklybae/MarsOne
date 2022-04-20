557b2cfad95aafc6c57b2c738c4894478bb2ca8d
Maksim Klychkov <maxim.klychkov64@gmail.com>
2022-04-20 22:34:06 +0300

    Authorisation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import datetime
 from flask import Flask, url_for, request, render_template, redirect
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, login_required, logout_user
 from data import db_session
 from data.job import Job
 from data.users import User
+from forms.login import LoginForm
 from forms.user import RegisterForm
 
 app = Flask(__name__)
@@ -53,6 +54,28 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 def main(db_name):
